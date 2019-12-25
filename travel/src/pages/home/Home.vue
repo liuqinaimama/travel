cfef9fa9cdd0538e160d43058b379b40c3b55546
liuqin <liuiqn@css.com.cn>
2019-12-25 17:08:37 +0800

    11

---
+++ travel/src/pages/home/Home.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class='home'>
+    <!-- <img src="./assets/logo.png"> -->
+    home
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+name: 'Home'
+}
+</script>
+<style>
+    .home {
+        font-size:50px;
+    }
+</style>
